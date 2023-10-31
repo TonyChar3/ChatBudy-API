907e35a7eb46bf75a3766c4684376471a39414db
Anthony <charetteanthony@outlook.com>
2023-10-31 13:26:20 -0400

    Fixed corsOption function error

--- middleware/getOrigins.js
+++ middleware/getOrigins.js
@@ -1,12 +1,27 @@
-import { VerifyOriginHeader } from "./authHandle.js";
-
+import Widget from '../models/widgetModels.js';
+/**
+ * Check for allowed origin domain
+ */
+const AllowedDomainVerification = async(req) => {
+    try{
+        const origin = req.headers.origin;
+        // check if the origin exist in the Widget collection
+        const verify_origin = await Widget.findOne({ domain: origin });
+        if(verify_origin === null){
+            throw new Error('Invalid origin');
+        }
+        return origin;
+    } catch(err){
+        return false;
+    }
+}
 /**
  * Function to set the cors middleware options
  */
-const corsOptions = async(req,res,callback ) => {
+const corsOptions = async(req,callback ) => {
     try{
-        const verified_origin = await VerifyOriginHeader(req,res);
-        if(verified_origin === null){
+        const verified_origin = await AllowedDomainVerification(req);
+        if(verified_origin === null || !verified_origin){
             return
         }
         callback(null, { origin: verified_origin, credentials: true });
