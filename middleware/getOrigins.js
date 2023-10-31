e23007db73cc91724bb54609499f4e53e87e4c31
Anthony <charetteanthony@outlook.com>
2023-10-31 13:15:19 -0400

    Fixed the corsOption to handle httpOnly cookie

--- middleware/getOrigins.js
+++ middleware/getOrigins.js
@@ -1,10 +1,15 @@
+import { VerifyOriginHeader } from "./authHandle";
 
 /**
  * Function to set the cors middleware options
  */
-const corsOptions = async(req, callback ) => {
+const corsOptions = async(req,res,callback ) => {
     try{
-        callback(null, { origin: '*', credentials: true });
+        const verified_origin = await VerifyOriginHeader(req,res);
+        if(verified_origin === null){
+            return
+        }
+        callback(null, { origin: verified_origin, credentials: true });
     } catch(err){
         console.log(err)
     }
