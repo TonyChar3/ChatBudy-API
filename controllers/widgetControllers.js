c622ac66aae92416c5a3e3a241cf85c655c3112a
Anthony <charetteanthony@outlook.com>
2023-11-01 15:03:00 -0400

    Fix for sse cookie

--- controllers/widgetControllers.js
+++ controllers/widgetControllers.js
@@ -112,6 +112,7 @@ const widgetSSEAuth = asyncHandler( async(req,res,next) => {
 // TODO: Add a dynamic Access-Control-Allow-Origin domain for production
 const widgetSSEConnection = asyncHandler(async(req,res,next) => {
     try{
+        console.log(connect_sse)
         if(connect_sse){
             const origin = req.headers('Origin');
             console.log('SSE widhet', origin)
