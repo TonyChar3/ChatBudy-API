f69eb970c6e10f7d1987b7e2d2c83c189b8b7dcf
Anthony <charetteanthony@outlook.com>
2023-11-01 12:02:54 -0400

    trying to fix the usage of httpOnly cookie

--- middleware/authHandle.js
+++ middleware/authHandle.js
@@ -24,6 +24,7 @@ const VerifyFirebaseToken = async(req,res) => {
 const VerifyAccessWidgetStyle = async(req,res) => {
     // const token = req.headers.authorization.split(" ")[1]
     const token = req.cookies;
+    console.log(req);
     const decode_token = await decodeJWT(token, 'Visitor');
     if(Object.keys(decode_token).length === 0){
         await VerifyFirebaseToken(req,res);
