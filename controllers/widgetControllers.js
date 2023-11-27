84f98b369350f6160f37ebcc21c9a1ddaf72c904
Anthony <charetteanthony@outlook.com>
2023-11-27 08:46:49 -0500

    FIX visitor notification not updating on  SSE close

--- controllers/widgetControllers.js
+++ controllers/widgetControllers.js
@@ -124,6 +124,7 @@ const widgetSSEConnection = asyncHandler(async(req,res,next) => {
         sse_connections.set(connect_sse.id, res);
         sendVisitorNotification(connect_sse.user_access, connect_sse.id);
         sendWidgetAdminStatus(connect_sse.user_access, connect_sse.id);
+        console.log(sse_connections.get(connect_sse.id));
         // clean up if the connection is closed or if an error occurs
         res.on("error", (error) => {
             custom_err_message = `${error.message}`
