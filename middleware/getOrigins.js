7d365498bb2b8983fa9d303ce7af6dd1c2dd9f0e
Anthony <charetteanthony@outlook.com>
2023-11-28 14:06:45 -0500

    FIX access for the admin panel

--- middleware/getOrigins.js
+++ middleware/getOrigins.js
@@ -5,11 +5,9 @@ import Widget from '../models/widgetModels.js';
 const AllowedDomainVerification = async() => {
     try{ 
         const domains = await Widget.distinct('domain');
-        domains.push('http://localhost:5173');// allow access to the admin panel as well
+        domains.push('https://www.chatbudy.io/');
         domains.push('http://localhost:3000');
         domains.push('http://10.0.0.78:3000');
-        domains.push('https://iran-mediterranean-reservation-handled.trycloudflare.com')
-        domains.push('https://tony-web-store.myshopify.com')
         domains.push('https://chatbudy-api.onrender.com')
         return domains
     } catch(err){
