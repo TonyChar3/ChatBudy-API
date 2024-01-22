6631aa723168d194d05e7911c26df7132519d757
Tony Char3 <tonyc6731@gmail.com>
2024-01-22 08:27:35 -0500

    UPDATE: added new domains to allow localhost of the frontend to go through

--- middleware/getOrigins.js
+++ middleware/getOrigins.js
@@ -22,25 +22,24 @@ const AllowedDomainVerification = async() => {
  */
 const corsOptions = async(req,callback ) => {
     try{
-        // const allowedOrigins = await AllowedDomainVerification();
-        // const origin = req.header('Origin');
-        // const host = req.header('host');
-        // const stripe_headers = req.rawHeaders.includes('Stripe-Signature')
-        // if(allowedOrigins && origin){
-        //     const matchingOrigin = allowedOrigins.find((allowedOrigin) => {
-        //         return origin.toString() === allowedOrigin.toString();
-        //     });
-        //     if(matchingOrigin) {
-        //         callback(null, { origin: matchingOrigin, credentials: true });
-        //     } else if(host === 'chatbudy-api.onrender.com' || host === 'localhost:8080'){
-        //         callback(null, { origin: '*', credentials: true });
-        //     } else {
-        //         callback(new Error('Not allowed by CORS'));
-        //     }
-        // } else if (stripe_headers && host === 'chatbudy-api.onrender.com' || host === 'localhost:8080') {
-        //     callback(null, { origin: `${host === 'localhost:8080'? 'localhost:8080' : 'chatbudy-api.onrender.com'}`, credentials: true });
-        // }
-        callback(null, { origin: '*', credentials: true });
+        const allowedOrigins = await AllowedDomainVerification();
+        const origin = req.header('Origin');
+        const host = req.header('host');
+        const stripe_headers = req.rawHeaders.includes('Stripe-Signature')
+        if(allowedOrigins && origin){
+            const matchingOrigin = allowedOrigins.find((allowedOrigin) => {
+                return origin.toString() === allowedOrigin.toString();
+            });
+            if(matchingOrigin) {
+                callback(null, { origin: matchingOrigin, credentials: true });
+            } else if(host === 'chatbudy-api.onrender.com' || host === 'localhost:8080'){
+                callback(null, { origin: host, credentials: true });
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        } else if (stripe_headers && host === 'chatbudy-api.onrender.com' || host === 'localhost:8080') {
+            callback(null, { origin: `${host === 'localhost:8080'? 'localhost:8080' : 'chatbudy-api.onrender.com'}`, credentials: true });
+        }
     } catch(err){
         console.log(err)
     }
