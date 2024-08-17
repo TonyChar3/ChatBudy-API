2ea2c556dafab7e35435e580d3c45c3251a5a999
Tony Char3 <tonyc6731@gmail.com>
2024-08-16 21:52:41 -0400

    FIX: Update package and bug fixe

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,19 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 /**
  * Function to connect the mongoDB database
  */
-const connectDB = async() => {
-    try{
-        // connect to the DB using the connection string
-        const connect = await mongoose.connect(process.env.DB_STRING, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("DB connected", connect.connection.host, connect.connection.name);
-    } catch(err){
-        console.log(err);
-        process.exit(1);
-    }
-}
+const connectDB = async () => {
+  try {
+    // connect to the DB using the connection string
+    const connect = await mongoose.connect(process.env.DB_STRING);
+    console.log(
+      "DB connected",
+      connect.connection.host,
+      connect.connection.name
+    );
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
